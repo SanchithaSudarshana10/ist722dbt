3e56ffbf3a566082c82488704c171f04216520aa
sbsudars@g.syr.edu <sbsudars@g.syr.edu>
2025-11-08 00:11:31 +0000

    commit

--- quickstart/models/customers.sql
+++ quickstart/models/customers.sql
@@ -1,18 +1,8 @@
-
-
-{{ 
-    config(
-        materialized='view'
-    ) 
+{{
+  config(
+    materialized='view'
+  )
 }}
-
-with customers as (
-
-    select
-        id as customer_id
-        ...
-
-)
 
 
 with customers as (
